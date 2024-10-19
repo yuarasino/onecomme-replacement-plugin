aa3ccaaf7a9051bccaacfe6ecb1d43fb982283d0
yuarasino <37145593+yuarasino@users.noreply.github.com>
2024-10-19 23:32:23 +0900

    :wrench: Bun:workspacesを導入

---
+++ packages/common/src/consts.ts
@@ -0,0 +1,6 @@
+export const PLUGIN_NAME = "わんコメテキスト置換プラグイン"
+export const PLUGIN_UID = "net.yuarasino.onecomme-replacement-plugin"
+export const PLUGIN_VERSION = "0.0.0"
+export const PLUGIN_AUTHOR = "新篠ゆう"
+export const WEB_ENDPOINT = `http://localhost:11180/plugins/${PLUGIN_UID}`
+export const API_ENDPOINT = `http://localhost:11180/api/plugins/${PLUGIN_UID}`
