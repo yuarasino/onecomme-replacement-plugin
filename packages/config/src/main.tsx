1b1924cbcc879b2f1a2f7edb1462c6325854f222
yuarasino <37145593+yuarasino@users.noreply.github.com>
2024-10-20 02:02:13 +0900

    :gear: MUIを導入

--- packages/config/src/main.tsx
+++ packages/config/src/main.tsx
@@ -1,12 +1,19 @@
+import CssBaseline from "@mui/material/CssBaseline"
+import { ThemeProvider } from "@mui/material/styles"
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
-import App from "./App.tsx"
+
+import App from "./App"
+import theme from "./theme"
 
 const elem = document.getElementById("root")
 if (!elem) throw Error()
 const root = createRoot(elem)
 root.render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
---
+++ packages/config/src/theme.tsx
@@ -0,0 +1,10 @@
+import { createTheme } from "@mui/material/styles"
+import '@fontsource-variable/noto-sans-jp';
+
+
+export default createTheme({
+  cssVariables: true,
+  typography: {
+    fontFamily: "'Noto Sans JP Variable', sans-serif",
+  },
+})
